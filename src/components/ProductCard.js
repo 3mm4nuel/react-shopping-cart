c689864ae8dcb1a26a075f05314266309eded89f
Emmanuel Chu Chung Chuck <Emmanuel.ChuChungChuck@reece.com.au>
2021-10-01 15:17:14 +1000

    Implemented increment/decrement functionality for item quantity

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -20,14 +20,44 @@ const Button = styled.button`
     cursor: pointer;
     position: absolute;
     top: 80px;
-    left: 670px;
+    left: 700px;
+`
+
+const StyledQuantityModifier = styled(QuantityModifier)`
+    position: absolute;
+    top: 50px;
+    left: 700px;
+
 `
 
 export default function ProductCard({ product, removeHandler}) {
+    const [quantity, setQuantity] = React.useState(product.quantity)
+
+    function incrementQuantity() {
+        setQuantity(prevQuantity => prevQuantity + 1)
+    }
+
+    function decrementQuantity() {
+        setQuantity(prevQuantity => prevQuantity - 1)
+    }
+
+
     return (
       <Li> 
         {product.value}
-         <Button onClick={() => removeHandler(product.id)}>remove</Button>
+        <Button onClick={() => removeHandler(product.id)}>remove</Button>
+        <StyledQuantityModifier quantity={quantity} incrementHandler={incrementQuantity} decrementHanlder={decrementQuantity}></StyledQuantityModifier>
       </Li>
+    );
+}
+
+
+function QuantityModifier({className, quantity, incrementHandler, decrementHanlder}) {
+    return (
+    <div className={className}>
+        <button onClick={decrementHanlder}>-</button>
+        <span>{quantity}</span>
+        <button onClick={incrementHandler}>+</button>
+    </div>
     );
 }
--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 export const data = [
-    {id: 'a', value: 'Apple'}, 
-    {id: 'm', value: 'Mango'}, 
-    {id: 'o', value: 'Orange'}, 
-    {id: 'b', value: 'Banana'}, 
-    {id: 'k', value: 'Kiwi'}
+    {id: 'a', value: 'Apple', quantity: 1}, 
+    {id: 'm', value: 'Mango', quantity: 1}, 
+    {id: 'o', value: 'Orange', quantity: 1}, 
+    {id: 'b', value: 'Banana', quantity: 1}, 
+    {id: 'k', value: 'Kiwi', quantity: 1}, 
 ];
